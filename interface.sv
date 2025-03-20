0c49af2716b22fda7125c792a0d9c66f6473f469
ee658class <ee658_fall24@outlook.com>
2025-03-19 23:57:09 -0400

    add packet and interface

--- interface.sv
+++ interface.sv
@@ -4,10 +4,10 @@ Date created: 18 March 2025
 Description: interface component
 ====================================*/
 
-interface our_interface();
+interface our_interface(input logic clk);
 
-    //input_1
-    //input_2
-    
-    //output_1
+    logic [7:0] input_1;
+    logic [7:0] input_2;
+
+    logic [15:0] output_3;
 endinterface
